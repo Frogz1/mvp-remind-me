1bbcce73542ba47229f4b12d28c3ad6a248d88bf
Trevor Old <trevor.old@outlook.com>
2018-03-31 12:46:20 -0700

    project setup

---
+++ src/server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const jsonParser = bodyParser.json();
+
+app.use(jsonParser());
+
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const APP_DIR = path.resolve(__dirname, 'src/client');
+const BUILD_DIR = path.resolve(__dirname, 'public');
+
+const config = {
+  entry: `${APP_DIR}/index.jsx`,
+  output: {
+    path: BUILD_DIR,
+  filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.jsx', '.js']
+  },
+};
+
+module.exports = config;
