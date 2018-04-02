ef9ed31bf537b78d3ac7d2184029343e33c0af5f
Trevor Old <trevor.old@outlook.com>
2018-04-01 19:25:42 -0700

    Decided to only allow for reminders with the next hour

--- src/client/components/ReminderForm.jsx
+++ src/client/components/ReminderForm.jsx
@@ -2,47 +2,76 @@ import React from 'react';
 import { withStyles } from 'material-ui/styles';
 import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
+import { Input, Form, Container, FormButton, Select } from 'semantic-ui-react';
+import moment from 'moment';
 
-const styles = theme => ({
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
+const options = [
+  {
+    key: '1',
+    text: 'One Minutes',
+    value: 1,
   },
-  textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-    width: 200,
+  {
+    key: '5',
+    text: 'Five Minutes',
+    value: 5,
+  }, {
+    key: '10',
+    text: 'Ten Minutes',
+    value: 10,
+  }, {
+    key: '15',
+    text: 'Fifteen Minutes',
+    value: 15,
   },
-});
+  {
+    key: '30',
+    text: 'Thirty Minutes',
+    value: 30,
+  },
+  {
+    key: '45',
+    text: 'Forty-five Minutes',
+    value: 45,
+  },
+  {
+    key: '60',
+    text: 'One Hour',
+    value: 60,
+  },
+];
 
-const ReminderForm = (props) => {
-  const { classes } = props;
-  return (
-    <div className={classes.container}>
-      <TextField
-        label="Email"
-        id="margin-normal"
-        defaultValue=""
-        className={classes.textField}
-        helperText="e.g. jane@doe.com"
-        margin="normal"
-      />
-      <TextField
-        label="Reminder"
-        id="margin-normal"
-        defaultValue=""
-        className={classes.textField}
-        helperText="e.g. pick up billy at school"
-        margin="normal"
-      />
-      <Button
-        variant="raised"
-        color="primary"
-      >
-        Save
-      </Button>
-    </div>
-  );
-};
+const ReminderForm = props => (
 
-export default withStyles(styles)(ReminderForm);
+  <Container textAlign="center">
+    <Form>
+      <Form.Group>
+        <Form.Input width={6} label="Enter email" type="email" />
+      </Form.Group>
+      <Form.Group>
+        <Form.Select
+          fluid
+          label="Remind me in"
+          width={6}
+          options={options}
+          placeholder="5 minutes" 
+        />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.TextArea
+          width={6}
+          label="Reminder"
+          placeholder="What do you want to be reminded about?" 
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Button width={6} positive>Submit</Form.Button>
+      </Form.Group>
+    </Form>
+
+  </Container>
+
+);
+
+export default ReminderForm;
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,50 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from 'material-ui/Button';
-import TextField from 'material-ui/TextField'
-import Grid from 'material-ui/GridList'
-import EndingSoon from './components/EndingSoon.jsx';
-import ReminderForm from './components/ReminderForm.jsx';
+import {Header, Container} from 'semantic-ui-react';
+import EndingSoon from './components/EndingSoon';
+import ReminderForm from './components/ReminderForm';
+import moment from 'moment';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       reminders: [],
-    }
+      moment: moment()
+    };
     this.getTasksEndingSoon();
   }
 
   getTasksEndingSoon() {
-    return fetch('/v1/reminders')
-      .then((data) => {
-        data.json()
-          .then((data) => {
-            this.setState({
-              reminders: data
-            })
-          })
-      })
-      .catch(error => error)
+    return fetch('/v1/reminders').then((data) => {
+      data
+        .json()
+        .then((data) => {
+          this.setState({reminders: data});
+        });
+    }).catch(error => error);
   }
-  
-  
 
   render() {
     return (
       <div>
 
-    <Grid container="true" style={{"display":"flex", "flexWrap":"flex", "alignItem":"true"}}>
-      <Grid item='true' direction="column" justify="center" md={12}>
-        <ReminderForm />
-        </Grid>
-        </Grid>
-    
-        <EndingSoon reminders={this.state.reminders} />
-</div>
+        <Header size="huge">Remind-Me!</Header>
+        <ReminderForm/>
 
+        <EndingSoon reminders={this.state.reminders}/>
 
+      </div>
     );
   }
 }
 
-export default ReactDOM.render(<App />, document.getElementById('remind-me'));
+export default ReactDOM.render(
+  <App/>, document.getElementById('remind-me'));
